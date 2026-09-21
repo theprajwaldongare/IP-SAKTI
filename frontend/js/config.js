bbb595bbdc323c7048dd1c022c4e191447d551a7
Prajwal <prajwaldongare112@gmail.com>
2026-09-21 14:42:30 +0530

    config url fix

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -5,5 +5,5 @@ const CONFIG = {
     // Put your Render URL here when you deploy, e.g., "https://ip-sakti-backend.onrender.com"
     // Leave it as "http://127.0.0.1:8000" for local testing
     // API_BASE_URL: "https://ip-sakti-9cf2c.containers.snapdeploy.app" 
-    API_BASE_URL: "https://ip-sakti-swart.vercel.app/api"
+    API_BASE_URL: "https://ip-sakti-swart.vercel.app"
 };
